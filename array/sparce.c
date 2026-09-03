8114cafe6b615e882fce63b890a194aeeacf8c37
Debasish-Nayak-git <debasishn185@gmail.com>
2026-09-03 10:41:06 +0530

    adding the modified transpose form

--- array/sparce.c
+++ array/sparce.c
@@ -6,6 +6,22 @@ void input(int mat[20][20],int r,int c)
          scanf("%d",&mat[i][j]);
       }
 }
+void tran_trip(int Tran[20][3],int s[20][3],int c)
+{int i,j,k=0;
+Tran[0][0]=s[0][1];
+Tran[0][1]=s[0][0];
+Tran[0][2]=s[0][2];
+for( i=0;i<c;i++)
+{for(j=0;j<=s[0][2];j++)
+if(s[j][1]==i)
+{k=k+1;
+Tran[k][0]=s[j][1];
+Tran[k][1]=s[j][0];
+Tran[k][2]=s[j][2];
+}
+}
+    
+}
 int creat_triplet(int mat[20][20],int r,int c,int s[20][3])
 {int i,j,t=0;
  for(i=0;i<r;i++)
@@ -48,6 +64,10 @@ printf("Sparse matrix:\n");
 display(mat,r,c);
 int t=creat_triplet(mat,r,c,s);
 printf("Triplet of Sparse matrix:\n");
-display_sparce(s,t,3);
+display_sparce(s,t+1,3);
+int tran_triplet[20][3];
+tran_trip(tran_triplet,s,c);
+printf("Transpose Triplet of Sparse matrix:\n");
+display_sparce(tran_triplet,t+1,3);
 return 0;
 }
